f3f96c9ba7c6b602006c71138c1a3e15e7141d39
loe831122 <loe831122@gmail.com>
2020-02-19 14:59:47 +0800

    加上註解

--- AV.py
+++ AV.py
@@ -17,12 +17,10 @@ class AV:
         self.Video_Page_Thread_List = []
         self.Video_Page_Links = []
         self.Loading_Video_Page_Links()#讀取已保存的影片頁面列表
-
         self.Video_Download_Links = []
         self.Video_Download_Links_Queue = Queue()
         self.Video_Download_Links_Queue_Thread_Num = 50  # 最大爬蟲線程數
         self.Loading_Video_Download_Link() #讀取已保存的影片下載連結
-
         print('')
 
     def Download_Video(self,url):
@@ -36,42 +34,41 @@ class AV:
         self.Video_Download_Links = data
 
     def Get_Video_Download_Link(self,link):
-        header = "Host: airav.cc###Connection: keep-alive###Upgrade-Insecure-Requests: 1###User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36###Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8###Accept-Encoding: gzip, deflate, br###Accept-Language: zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7"
+        header = "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9###accept-encoding: gzip, deflate, br###accept-language: zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7,ja;q=0.6,zh-CN;q=0.5###cache-control: max-age=0###cookie: ASP.NET_SessionId=c2imwxq4pouvskcujykqbvca; __cfduid=df14915307877e38f662dace4a5d2bb4f1582077741; _ga=GA1.3.817680150.1582077744; _gid=GA1.3.1534557918.1582077744; _gat_gtag_UA_18029176_4=1; _gat_gtag_UA_9475216_1=1###referer: https://www.google.com/###sec-fetch-dest: document###sec-fetch-mode: navigate###sec-fetch-site: cross-site###sec-fetch-user: ?1###upgrade-insecure-requests: 1###user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36"
         rs = self.Net.Get(url=link,header_string=header)
         data = rs.content.decode()
+
+        #用正規找要得連結
         Get_3gp_Link = self.Net.preg_get_word('file: "(https://.+\.3gp)"',1,data)
         return Get_3gp_Link
 
     def Thread_Video_Download_Link(self):
+        #查看Q是不是空的
         while self.Video_Download_Links_Queue.qsize() != 0:
+            #取出Q的值
             link = self.Video_Download_Links_Queue.get()
+
             download_link = self.Get_Video_Download_Link(link)
             self.Video_Download_Links.append(download_link)
-
             time.sleep(0.2)
+    
 
+    #從每頁把所有影片的連結都保存下來
     def Get_Video_Download_Link_Start_Thread(self):
-
         # for Page_Link in tqdm(self.Video_Page_Links,desc="分配下載連結任務"):
         #     self.Video_Download_Links_Queue.put(Page_Link)
-
-
         for Page_Link in self.Video_Page_Links[0:300]:
+            #將所有連結都放到Q裡面
             self.Video_Download_Links_Queue.put(Page_Link)
-
-
-        for n in range(self.Video_Download_Links_Queue_Thread_Num):
-
+        for n in range(self.Video_Download_Links_Queue_Thread_Num):#=50
             t = Thread(target=self.Thread_Video_Download_Link)
             t.start()
         print("開始進行獲取影片")
-        Total_Mission = self.Video_Download_Links_Queue.qsize()
+        Total_Mission = self.Video_Download_Links_Queue.qsize() 
 
         while self.Video_Download_Links_Queue.qsize() != 0:
             print("影片連結獲取進度：{}/{}".format(self.Video_Download_Links_Queue.qsize(), Total_Mission))
-
             time.sleep(1)
-
         fp = open("Video_Download_Link.txt","w+")
         fp.write(json.dumps(self.Video_Download_Links))
         fp.close()
@@ -89,25 +86,21 @@ class AV:
 
 
 
-
+    #抓出總共有幾頁
     def Get_Page_Max_Number(self):
         url = "https://airav.cc/index.aspx?idx=1"
-        header = "Host: airav.cc###Connection: keep-alive###Cache-Control: max-age=0###Upgrade-Insecure-Requests: 1###User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36###Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8###Accept-Encoding: gzip, deflate, br###Accept-Language: zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7"
+        header = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9###Accept-Encoding: gzip, deflate, br###Accept-Language: zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7,ja;q=0.6,zh-CN;q=0.5###Cache-Control: max-age=0###Connection: keep-alive###Cookie: UM_distinctid=1705b875f85cf-0cb729bec7926a-b791b36-1fa400-1705b875f86137; CNZZDATA4000003=cnzz_eid%3D770345665-1582080959-https%253A%252F%252Flink.zhihu.com%252F%26ntime%3D1582080959; Hm_lvt_536e798a6b4ff16f87e8fbebde347f50=1582083564; Hm_lpvt_536e798a6b4ff16f87e8fbebde347f50=1582083961; sid=26724835.422929572.1582083715196.1582083961729###Host: one-piece.cn###If-Modified-Since: Fri, 14 Feb 2020 06:23:45 GMT###If-None-Match: 802e2a4fffe2d51:0###Sec-Fetch-Dest: document###Sec-Fetch-Mode: navigate###Sec-Fetch-Site: none###Sec-Fetch-User: ?1###Upgrade-Insecure-Requests: 1###User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36"
         rs = self.Net.Get(url=url, header_string=header)
         data = rs.content.decode()
-
         py_txt = pq(data).find(".nextback")
-
         Last_Page_Num_Ele = py_txt.eq(len(py_txt) -1 ).html()
-
         Last_Page_Num = pq(Last_Page_Num_Ele).attr('href').replace("/index.aspx?idx=","")
-
         return int(Last_Page_Num)
 
 
     def Get_Page_Video(self,page_num):
         url = "https://airav.cc/index.aspx?idx="+format(page_num)
-        header = "Host: airav.cc###Connection: keep-alive###Cache-Control: max-age=0###Upgrade-Insecure-Requests: 1###User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36###Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8###Accept-Encoding: gzip, deflate, br###Accept-Language: zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7"
+        header = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9###Accept-Encoding: gzip, deflate, br###Accept-Language: zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7,ja;q=0.6,zh-CN;q=0.5###Cache-Control: max-age=0###Connection: keep-alive###Cookie: UM_distinctid=1705b875f85cf-0cb729bec7926a-b791b36-1fa400-1705b875f86137; CNZZDATA4000003=cnzz_eid%3D770345665-1582080959-https%253A%252F%252Flink.zhihu.com%252F%26ntime%3D1582080959; Hm_lvt_536e798a6b4ff16f87e8fbebde347f50=1582083564; Hm_lpvt_536e798a6b4ff16f87e8fbebde347f50=1582083961; sid=26724835.422929572.1582083715196.1582083961729###Host: one-piece.cn###If-Modified-Since: Fri, 14 Feb 2020 06:23:45 GMT###If-None-Match: 802e2a4fffe2d51:0###Sec-Fetch-Dest: document###Sec-Fetch-Mode: navigate###Sec-Fetch-Site: none###Sec-Fetch-User: ?1###Upgrade-Insecure-Requests: 1###User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36"
         rs = self.Net.Get(url=url,header_string=header)
         data = rs.content.decode()
         List_Item = pq(data).find('.listItem')
@@ -143,7 +136,7 @@ class AV:
 
         Total_Mission = self.Video_Page_Queue.qsize()
         while self.Video_Page_Queue.qsize() != 0:
-            print("爬蟲進度：{}/{}".format(self.Video_Page_Queue.qsize(),Total_Mission))
+            print("抓取每頁連結爬蟲進度：{}/{}".format(self.Video_Page_Queue.qsize(),Total_Mission))
             time.sleep(1)
 
         f = open("Video_Page_Link.txt",'w+')
@@ -151,16 +144,10 @@ class AV:
         f.close()
 
 
-
-
-
-
-
-
 if __name__ == "__main__":
     obj = AV()
-    # obj.Get_Video_Download_Link(obj.Video_Page_Links[1])
+    obj.Get_Video_Download_Link(obj.Video_Page_Links[1])
     # obj.Get_Video_Download_Link_Start_Thread()
     # print(obj.Video_Download_Links)
-    obj.Download_Video(obj.Video_Download_Links[0])
+    #obj.Download_Video(obj.Video_Download_Links[0])
     # obj.Scrape_All_Video_Page_Link()
